f00c043de63e699deafe29415c0f048011766058
Aditya Maurya <mauryaaditya7405@gmail.com>
2024-05-17 13:22:17 +0530

    refactor: remove unnecessary navigation links from navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,23 +12,7 @@ const navbar = () => {
           <div className=" m-8 flex flex-shrink-0 items-center"> 
             <img className="mx-5 w-20 " src={icon} alt="icon"  />
           </div>
-          <div className="flex items-center justify-center gap-10 ">
-          <a href="#About-Me" className="flex flex-shrink-0 items-center hover:text-neutral-400 scroll-smooth">
-                      About
-          </a>
-          <div className="flex flex-shrink-0 items-center hover:text-neutral-400"> 
-            Technologies
-          </div>
-          <div className="flex flex-shrink-0 items-center hover:text-neutral-400"> 
-            Experience
-          </div>
-          <div className="flex flex-shrink-0 items-center  hover:text-neutral-400"> 
-            Projects
-          </div>
-          <div className="flex flex-shrink-0 items-center  hover:text-neutral-400"> 
-            Contact Me
-          </div>
-          </div>
+         
           
           <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <FaLinkedinIn/>
